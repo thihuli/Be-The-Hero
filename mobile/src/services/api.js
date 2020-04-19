db678b8bc95280dc30827e9a27407e19365cfe6b
thihuli <thiagocandidof@hotmail.com>
2020-04-18 23:52:13 -0300

    subindo aplicação mobile

---
+++ mobile/src/services/api.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
+
+const api = axios.create({
+    baseURL: 'http://192.168.0.107:3333'
+})
+
+
+export default api;
